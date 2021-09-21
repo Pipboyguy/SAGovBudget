3a020e262f644b399230c9d3975a4668a6a0e8be
Marcel <marcelc@nedbank.co.za>
2021-09-21 21:29:23 +0200

    Rename dockerfile

--- create-sqlitedb.sh
+++ create-sqlitedb.sh
@@ -3,7 +3,7 @@
 ### build and run notebook to create SQLite DB ###
 
 # build ETL notebook image
-docker build -t govnotebook -f govnotebook.df . 
+docker build -t govnotebook -f govnotebook.dockerfile . 
 # start container in background
 docker run  --name govnb -d -v `pwd`:/home/jovyan/work govnotebook
 # run papermill notebook pipeline
